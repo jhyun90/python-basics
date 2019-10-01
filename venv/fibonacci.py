a7d83336778dc3d2d0d34792cb64906f8cda9865
jhyun90 <jhyun9005@gmail.com>
2019-10-01 01:53:50 -0700

    initial commit

---
+++ venv/fibonacci.py
@@ -0,0 +1,52 @@
+
+
+# Using Recursion
+# def fibonacci(n):
+#     if n == 0:
+#         return 0
+#
+#     elif n == 1:
+#         return 1
+#
+#     else:
+#         return fibonacci(n-1) + fibonacci(n-2)
+
+
+# Using Dynamic Programming 1
+# def fibonacci(n):
+#     fib_arr = [0, 1]
+#
+#     if n <= 1:
+#         return
+#
+#     for i in range(2, n + 1):
+#         fib_arr.append(fib_arr[i - 1] + fib_arr[i - 2])
+#         # fib_arr[i] = fib_arr[n - 1] + fib_arr[n - 2]
+#
+#     return fib_arr
+#         # return fibonacci(n - 1) + fibonacci(n - 2)
+#
+# n = int(input())
+# f = fibonacci(n)
+# print(f[-1])
+
+
+# Using Dynamic Programming 2
+fib_arr = [0, 1]
+
+def fibonacci(n):
+    if n < 0:
+        return
+
+    # elif n <= len(fib_arr):
+    elif n <= 2:
+        return fib_arr[n - 1]
+
+    else:
+        # temp_fib = fibonacci(n - 1) + fibonacci(n - 2)
+        # fib_arr.append(fibonacci(n - 1) + fibonacci(n - 2))
+
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+n = int(input())
+print(fibonacci(n))
