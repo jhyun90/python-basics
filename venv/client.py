e9037c4e4bb9efda98dfaf1fa478d35042babd5b
jhyun90 <jhyun9005@gmail.com>
2019-10-03 08:03:50 -0700

    initial commit

--- venv/client.py
+++ venv/client.py
@@ -1,5 +1,8 @@
 from socket import *
+from threading import *
+import time
 
+# 1
 # clientSock = socket(AF_INET, SOCK_STREAM)
 # clientSock.connect(('127.0.0.1', 8080))
 # print('연결 확인 됐습니다.')
@@ -10,16 +13,64 @@ from socket import *
 # data = clientSock.recv(1024)
 # print('받은 데이터 : ', data.decode('utf-8'))
 
+# 2
+# port = 8080
+#
+# clientSock = socket(AF_INET, SOCK_STREAM)
+# clientSock.connect(('127.0.0.1', port))
+# print('접속 완료')
+#
+# while 1:
+#     recvData = clientSock.recv(1024)
+#     print("상대방: ", recvData.decode('utf-8'))
+#
+#     sendData = input(">>> ")
+#     clientSock.send(sendData.encode('utf-8'))
+
+# 3
+# def send(sock):
+#     sendData = input(">>> ")
+#     sock.send(sendData.encode('utf-8'))
+#
+# def receive(sock):
+#     recvData = sock.recv(1024)
+#     print("상대방: ", recvData.decode('utf-8'))
+#
+#
+# port = 8080
+#
+# clientSock = socket(AF_INET, SOCK_STREAM)
+# clientSock.connect(('127.0.0.1', port))
+# print("접속 완료")
+#
+# while 1:
+#     receive(clientSock)
+#     send(clientSock)
+
+# 4
+def send(sock):
+    while 1:
+        sendData = input(">>> ")
+        sock.send(sendData.encode('utf-8'))
+
+def receive(sock):
+    while 1:
+        recvData = sock.recv(1024)
+        print("상대방: ", recvData.decode('utf-8'))
+
 
 port = 8080
 
 clientSock = socket(AF_INET, SOCK_STREAM)
 clientSock.connect(('127.0.0.1', port))
-print('접속 완료')
+print("접속 완료")
+
+sender = Thread(target=send, args=(clientSock,))
+receiver = Thread(target=receive, args=(clientSock,))
+
+sender.start()
+receiver.start()
 
 while 1:
-    recvData = clientSock.recv(1024)
-    print("상대방: ", recvData.decode('utf-8'))
-
-    sendData = input(">>> ")
-    clientSock.send(sendData.encode('utf-8'))
+    time.sleep(1)
+    pass
--- venv/server.py
+++ venv/server.py
@@ -1,5 +1,8 @@
 from socket import *
+from threading import *
+import time
 
+# 1
 # serverSock = socket(AF_INET, SOCK_STREAM)
 # serverSock.bind(('', 8080))
 # serverSock.listen(1)
@@ -13,6 +16,61 @@ from socket import *
 # connectionSock.send('I am a server.'.encode('utf-8'))
 # print('메시지를 보냈습니다.')
 
+# 2
+# port = 8080
+#
+# serverSock = socket(AF_INET, SOCK_STREAM)
+# serverSock.bind(('', port))
+# serverSock.listen(1)
+#
+# print("{}번 포트로 접속 대기 중...".format(port))
+#
+# connectionSock, addr = serverSock.accept()
+# print(str(addr), "에서 접속이 확인되었습니다.")
+#
+# while 1:
+#     sendData = input(">>> ")
+#     connectionSock.send(sendData.encode('utf-8'))
+#
+#     recvData = connectionSock.recv(1024)
+#     print("상대방: ", recvData.decode('utf-8'))
+
+# 3
+# def send(sock):
+#     sendData = input(">>> ")
+#     sock.send(sendData.encode('utf-8'))
+#
+# def receive(sock):
+#     recvData = sock.recv(1024)
+#     print("상대방: ", recvData.decode('utf-8'))
+#
+#
+# port = 8080
+#
+# serverSock = socket(AF_INET, SOCK_STREAM)
+# serverSock.bind(('', port))
+# serverSock.listen(1)
+#
+# print("{}번 포트로 접속 대기 중...".format(port))
+#
+# connectionSock, addr = serverSock.accept()
+# print(str(addr), "에서 접속이 확인되었습니다.")
+#
+# while 1:
+#     send(connectionSock)
+#     receive(connectionSock)
+
+# 4
+def send(sock):
+    while 1:
+        sendData = input(">>> ")
+        sock.send(sendData.encode('utf-8'))
+
+def receive(sock):
+    while 1:
+        recvData = sock.recv(1024)
+        print("상대방: ", recvData.decode('utf-8'))
+
 
 port = 8080
 
@@ -25,9 +83,12 @@ print("{}번 포트로 접속 대기 중...".format(port))
 connectionSock, addr = serverSock.accept()
 print(str(addr), "에서 접속이 확인되었습니다.")
 
-while 1:
-    sendData = input(">>> ")
-    connectionSock.send(sendData.encode('utf-8'))
+sender = Thread(target=send, args=(connectionSock,))
+receiver = Thread(target=receive, args=(connectionSock,))
 
-    recvData = connectionSock.recv(1024)
-    print("상대방: ", recvData.decode('utf-8'))
+sender.start()
+receiver.start()
+
+while 1:
+    time.sleep(1)
+    pass
